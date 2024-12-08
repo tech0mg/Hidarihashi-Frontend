1b88fe25c05645893b7e43ff1e7666493e21f019
mukagen <tech0.mukainakano@gmail.com>
2024-12-08 19:55:44 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   output: 'standalone',
+  env: {
+    PORT: '3000', // 環境変数にポート番号を設定
+  },
 };
 
 module.exports = nextConfig;
