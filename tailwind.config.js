96f2491cfb844416663eb1e62a5c528d05a58157
mukagen <tech0.mukainakano@gmail.com>
2024-12-08 13:41:31 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: [
     "./stc/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/app/**/*.{js,jsx,ts,tsx}", // Next.jsの主要フォルダ
-    "./src/styles/**/*.{css}",        // グローバルCSSの場所
+    "./src/app/**/*.{js,jsx,ts,tsx}", // Next.jsの主要フォルダ      
+    "./src/styles/**/*.css",          // グローバルCSSの場所
     "./public/**/*.html",             // 必要なら静的HTMLファイルも含む
   ],
   theme: {
