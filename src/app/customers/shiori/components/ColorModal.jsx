d62345edb2392c577e480032c4c87012a2684f81
egoma17 <em.y.25852@gmail.com>
2024-12-12 02:53:33 +0900

    ColorModalの色の修正、TOPの企業リンクボタンカラー変更

--- src/app/customers/shiori/components/ColorModal.jsx
+++ src/app/customers/shiori/components/ColorModal.jsx
@@ -5,16 +5,15 @@ import { useColor } from "../../../context/ColorContext";
 const ColorModal = ({ onClose }) => {
   const { setColor } = useColor();
   const colors = [
-    { color: "#F28B82", label: "赤" },
-    { color: "#F06292", label: "ピンク" },
-    { color: "#CE93D8", label: "紫" },
-    { color: "#64B5F6", label: "青" },
-    { color: "#81C784", label: "緑" },
-    { color: "#E0E0E0", label: "グレー" },
-    { color: "#4DD0E1", label: "シアン" },
-    { color: "#FFD54F", label: "オレンジ" },
-    { color: "#FFF176", label: "黄色" },
-    { color: "#A1887F", label: "ブラウン" },
+    { color: "#E37E88", label: "赤" },
+    { color: "#DA7997", label: "ピンク" },
+    { color: "#C2AAC5", label: "紫" },
+    { color: "#5F72D1", label: "青" },
+    { color: "#389D63", label: "緑" },
+    { color: "#63C0C3", label: "シアン" },
+    { color: "#EFB97B", label: "オレンジ" },
+    { color: "#E4E872", label: "黄色" },
+    { color: "#9A877A", label: "ブラウン" },
   ];
 
   const handleColorChange = (color) => {
@@ -25,7 +24,7 @@ const ColorModal = ({ onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg">
-        <h2 className="text-xl font-bold mb-4 text-center">色を選択</h2>
+        <h2 className="text-xl font-bold mb-6 text-center">いろをえらぶ</h2>
         <div className="grid grid-cols-3 gap-4">
           {colors.map((colorOption, index) => (
             <button
@@ -38,10 +37,10 @@ const ColorModal = ({ onClose }) => {
           ))}
         </div>
         <button 
-        className="mt-4 p-2 bg-gray-500 text-white rounded-md" 
+        className="mt-6 p-2 bg-gray-400 text-white rounded-md" 
         onClick={onClose}
         >
-          閉じる
+          とじる
         </button>
       </div>
     </div>
