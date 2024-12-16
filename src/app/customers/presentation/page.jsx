9507df244fe69a4ee0d2f6c552c3682a8e4f3055
mukagen <tech0.mukainakano@gmail.com>
2024-12-16 20:50:38 +0900

    Update page.jsx

--- src/app/customers/presentation/page.jsx
+++ src/app/customers/presentation/page.jsx
@@ -1,25 +1,28 @@
+import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf';
 
-const [totalPages, setTotalPages] = useState(0);
-const apiUrl = process.env.NEXT_PUBLIC_API_URL; 
+const PDFViewer = () => {
+  const [totalPages, setTotalPages] = useState(0); // useStateをコンポーネント内で宣言
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-const onLoadSuccess = ({numPages}) => {
-  setTotalPages(numPages);
-}
+  const onLoadSuccess = ({ numPages }) => {
+    setTotalPages(numPages); // PDFの総ページ数を設定
+  };
 
-const pdfContents = [];
-for(let i=0; i<totalPages; i++){
-  pdfContents.push(
-    <Page key={i} pageNumber={i+1} width={300} />
+  const pdfContents = [];
+  for (let i = 0; i < totalPages; i++) {
+    pdfContents.push(
+      <Page key={i} pageNumber={i + 1} width={300} />
+    );
+  }
+
+  return (
+    <div style={{ width: "100%", height: "100%", overflow: 'scroll' }}>
+      <Document file={`${apiUrl}/PDFのURL`} onLoadSuccess={onLoadSuccess}>
+        {pdfContents}
+      </Document>
+    </div>
   );
-}
+};
 
-return (
-  <div style={{ width: "100%", height: "100%", overflow:'scroll' }}>
-    <Document file='PDFのURL' onLoadSuccess={onLoadSuccess}>
-      {pdfContents}
-    </Document>
-  </div>
-);
-
-
+export default PDFViewer;
